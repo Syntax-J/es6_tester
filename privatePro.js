3784fd1b96a0b96acb825484b62db2408858b4fc
Syntax-J <773818536@qq.com>
2018-08-29 15:05:11 +0800

    Create privatePro.js

---
+++ privatePro.js
@@ -0,0 +1,17 @@
+//Private properties in JavaScript ES6 classes
+//ES6
+
+class Person {
+    constructor(name) {
+        var _name = name
+        this.setName = function(name) { _name = name; }
+        this.getName = function() { return _name; }
+    }
+}
+//ES5
+
+function Person(name) {
+    var _name = name
+    this.setName = function(name) { _name = name; }
+    this.getName = function() { return _name; }
+}
