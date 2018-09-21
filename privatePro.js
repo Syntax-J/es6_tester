850d34a734535a142aff1c44c49e906dceb29bb6
Syntax-J <773818536@qq.com>
2018-09-21 15:52:15 +0800

    Revert "Update privatePro.js"
    This reverts commit eda01878e3586c067e906853546b138768a5c150.

--- privatePro.js
+++ privatePro.js
@@ -3,8 +3,8 @@
 
 class Person {
     constructor(name) {
-        var _name = name //private here
-        this.setName = function(name) { _name = name; } //public here 
+        var _name = name
+        this.setName = function(name) { _name = name; }
         this.getName = function() { return _name; }
     }
 }
